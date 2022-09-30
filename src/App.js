5b7b8e0c2bc44412f28fb9f79d7a54fecf1054d9
obiri job <obirijob20@gmail.com>
2022-09-30 13:02:31 +0300

    Changes in css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [text, setText] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mine">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(text);
+        }}
+      >
+        <input
+          onInput={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
